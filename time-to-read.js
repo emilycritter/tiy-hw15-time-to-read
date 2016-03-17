48ff735694cbc7083c17b0b63e3a69df9c7a9177
emily.c.ritter <emily.c.ritter@gmail.com>
2016-03-16 20:04:16 -0500

    Complete challenge 1.2

--- time-to-read.js
+++ time-to-read.js
@@ -1,14 +1,23 @@
-var fetchData = $('p').text();
-
-console.log(fetchData);
-
-
-var timeToRead = function(fetchData){
+$(window).ready(function(){
+  var fetchData = $('p').text();
   var wordsPerMinute = 230;
-  wordCount = fetchData.split(" ").length;
-  timeToRead = wordCount / wordsPerMinute;
-  timeToRead = Math.round(timeToRead);
-  return timeToRead;
-};
 
-console.log(timeToRead(fetchData));
+  var timeToRead = function(fetchData, wordsPerMinute){
+    wordCount = fetchData.split(" ").length;
+    timeToRead = wordCount / wordsPerMinute;
+    timeToRead = Math.round(timeToRead);
+    return timeToRead;
+  };
+
+  console.log("Approximately " + timeToRead(fetchData, wordsPerMinute) + " minutes to read");
+  // var approxTime = timeToRead(fetchData, wordsPerMinute);
+  readingTime = timeToRead;
+
+  var newHTML = '<p>' + readingTime + ' minutes to read</p>';
+  var $htmlTag = $('time-to-read-view');
+
+  console.log(newHTML);
+
+  $('.time-to-read-view').append(newHTML);
+
+});
